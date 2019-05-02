c7165449da6cb2d6d41cf65b615b6f3afdc4f76b
Irisedmonson <ie009416@scc-mmlab-17.local>
2019-05-02 13:42:28 -0400

    Added Final

---
+++ Edmonson_3401_Final/Assets/Scripts/AgentMotor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMotor : MonoBehaviour
+{
+    public float agentSpeed = 5.1f;
+    public Transform Player;
+    public NavMeshAgent Nav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Nav.speed = agentSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray, out hit))
+            {
+                Player.position = hit.point;
+            }
+        }
+    }
+
+
+
+
+}
